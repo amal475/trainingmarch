22916c8ae1d1d0c4843d12949e8e01a201b08c3f
amal <fathallahamal86@gmail.com>
2023-03-02 21:31:00 +0100

    update helloworld-springboot

---
+++ helloworld-springboot/src/main/java/us/demosqoin/demosqoin/DemosqoinApplication.java
@@ -0,0 +1,13 @@
+package us.demosqoin.demosqoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosqoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosqoinApplication.class, args);
+	}
+
+}
---
+++ helloworld-springboot/src/test/java/us/demosqoin/demosqoin/DemosqoinApplicationTests.java
@@ -0,0 +1,13 @@
+package us.demosqoin.demosqoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosqoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
